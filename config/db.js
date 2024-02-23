3bb3d1d47b13fef36dc52f9d6b22497a7dfdac57
LAICHEEHOONG <laicheehoong@gmail.com>
2024-02-23 22:14:23 +0800

    add port

--- config/db.js
+++ config/db.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect(process.env.MONGODB_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true
-    // });
-    await mongoose.connect('mongodb+srv://laicheehoong:mKQpuTCVC3KvWwnj@cluster0.6dczomc.mongodb.net/YoutubeUser?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+    // await mongoose.connect('mongodb+srv://laicheehoong:mKQpuTCVC3KvWwnj@cluster0.6dczomc.mongodb.net/YoutubeUser?retryWrites=true&w=majority&appName=Cluster0', {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true
+    // });
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ require('dotenv').config();
 
 // Connect to MongoDB
 // mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb+srv://laicheehoong:mKQpuTCVC3KvWwnj@cluster0.6dczomc.mongodb.net/YoutubeUser?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect('mongodb+srv://laicheehoong:mKQpuTCVC3KvWwnj@cluster0.6dczomc.mongodb.net/YoutubeUser?retryWrites=true&w=majority&appName=Cluster0');
 
 
 app.use(express.json());
@@ -21,4 +21,5 @@ app.use('/users', userRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
