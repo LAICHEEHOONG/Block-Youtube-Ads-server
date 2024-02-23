9998601495c18fe7c6dc27700c68129949fd2f53
LAICHEEHOONG <laicheehoong@gmail.com>
2024-02-23 21:54:15 +0800

    sdf

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    // await mongoose.connect(process.env.MONGODB_URI, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true
+    // });
+    await mongoose.connect('mongodb+srv://laicheehoong:mKQpuTCVC3KvWwnj@cluster0.6dczomc.mongodb.net/YoutubeUser?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ require('dotenv').config();
 
 // Connect to MongoDB
 // mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb+srv://laicheehoong:mKQpuTCVC3KvWwnj@cluster0.6dczomc.mongodb.net/YoutubeUser?retryWrites=true&w=majority&appName=Cluster0');
 
 
 app.use(express.json());
